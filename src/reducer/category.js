0911eaf3704ae6513a1da0b9bf83dd2471586163
Dustin <dyelland@gmail.com>
2017-10-02 23:04:52 -0500

    wrote the rest of the state tests

--- src/reducer/category.js
+++ src/reducer/category.js
@@ -6,6 +6,15 @@ export default (state = initialState,action = {}) => {
   switch(type){
     case 'CATEGORY_CREATE':
       return [...state,payload];
+    case 'CATEGORY_UPDATE':
+      return state.map(category => {
+        return category.id === payload.id ?
+          payload : category;
+      });
+    case 'CATEGORY_REMOVE':
+      return state.filter(category => {
+        return category.id !== payload.id;
+      });
     default:
       return state;
   }
--- src/reducer/category.test.js
+++ src/reducer/category.test.js
@@ -6,3 +6,64 @@ test('initial state should be an empty array',() => {
   let res = category();
   expect(res).toEqual([]);
 });
+
+test('invalid action should return current state',() => {
+  const state = [];
+  const action = {
+    type: 'UNKNOWN'
+  };
+
+  deepFreeze([state,action]);
+
+
+  const res = category(state,action);
+  expect(res).toEqual(state);
+});
+
+test('create category',() => {
+  const state = [{ title: 'Utilities' }];
+  const action = {
+    type: 'CATEGORY_CREATE',
+    payload: { title: 'Groceries' }
+  };
+
+  deepFreeze(state,action);
+
+  const res = category(state,action);
+  expect(res).toEqual([
+    { title: 'Utilities' },
+    { title: 'Groceries' }
+  ]);
+});
+
+test('update category',() => {
+  const state = [
+    { id: 0,title: 'Utilities' },
+    { id: 1,title: 'Groceries' }
+  ];
+  const action = {
+    type: 'CATEGORY_UPDATE',
+    payload: { id: 1,title: 'Food' }
+  };
+
+  deepFreeze([state,action]);
+
+  const res = category(state,action);
+  expect(res).toEqual([
+    { id: 0,title: 'Utilities'},
+    { id: 1,title: 'Food' }
+  ]);
+});
+
+test('remove category',() => {
+  const state = [{ id: 0,title: 'Food'}];
+  const action = {
+    type: 'CATEGORY_REMOVE',
+    payload: { id: 0 }
+  };
+
+  deepFreeze([state,action]);
+
+  const res = category(state,action);
+  expect(res).toEqual([]);
+});
